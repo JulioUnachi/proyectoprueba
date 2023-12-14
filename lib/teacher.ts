22c982b6d9af040a5c11e729ee5a3571e142af5d
Julio <julio.pea@unachi.ac.pa>
2023-12-13 19:55:40 -0500

    final commit

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,8 +1,11 @@
 export const isTeacher = (userId?: string | null) => {
   const teacherId1 = process.env.NEXT_PUBLIC_TEACHER_ID_1;
   const teacherId2 = process.env.NEXT_PUBLIC_TEACHER_ID_2;
+  const teacherId3 = process.env.NEXT_PUBLIC_TEACHER_ID_3;
+  const teacherId4 = process.env.NEXT_PUBLIC_TEACHER_ID_4;
+  const teacherId5 = process.env.NEXT_PUBLIC_TEACHER_ID_5;
   
-  return (userId === teacherId1 || userId === teacherId2 )
+  return (userId === teacherId1 || userId === teacherId2  || userId ===  teacherId3 || userId ===  teacherId4 || userId ===  teacherId5 )
 
 };
 
